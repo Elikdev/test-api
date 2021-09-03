18deea84e8bf69242ab73ab0fc7eea9b57bef797
boma25 <amakirisamuelboma@gmail.com>
2021-09-03 22:34:44 +0100

    chore: setup complete

--- ormconfig.js
+++ ormconfig.js
@@ -1,36 +1,32 @@
-const dotenv = require("dotenv");
+/** @format */
 
-if (process.env.NODE_ENV === 'production'){
-   dotenv.config() // .env file
-}else {
-   dotenv.config({ path: '.test.env'})
+const dotenv = require("dotenv")
+
+if (process.env.NODE_ENV === "production") {
+	dotenv.config() // .env file
+} else {
+	dotenv.config({ path: ".test.env" })
 }
 
-module.exports =  {
-   "type": "postgres",
-   "host": process.env.DB_HOST,
-   "port": 5432,
-   "username": process.env.DB_USER,
-   "password": process.env.DB_PASSWORD,
-   "database": process.env.DB_NAME,
-   "synchronize": false,
-   "logging": false,
-   "entities": [
-      "src/api/**/*.model.ts"
-   ],
-   "migrations": [
-      "src/db/migration/**/*.ts"
-   ],
-   "subscribers": [
-      "src/subscriber/**/*.ts"
-   ],
-   "cli": {
-      "migrationsDir": "src/db/migration",
-      "subscribersDir": "src/subscriber"
-   },
-   "extra": {
-      "ssl": {
-        "rejectUnauthorized": false
-      }
-    }
+module.exports = {
+	type: "postgres",
+	host: process.env.DB_HOST,
+	port: 5432,
+	username: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME,
+	synchronize: false,
+	logging: false,
+	entities: ["src/api/**/*.model.ts"],
+	migrations: ["src/db/migration/**/*.ts"],
+	subscribers: ["src/subscriber/**/*.ts"],
+	cli: {
+		migrationsDir: "src/db/migration",
+		subscribersDir: "src/subscriber",
+	},
+	// extra: {
+	// 	ssl: {
+	// 		rejectUnauthorized: false,
+	// 	},
+	// },
 }
