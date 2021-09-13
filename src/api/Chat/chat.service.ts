3abe87ff124437b684825add7a0cab54298dbe95
Odutoye Kolade Elijah <koladeodutoye9913@gmail.com>
2021-09-13 07:29:56 +0100

    chat and message model set up

---
+++ src/api/Chat/chat.service.ts
@@ -0,0 +1,33 @@
+import { getRepository } from "typeorm"
+import { BaseService, jwtCred } from "../../enums"
+import { User } from "../User/user.model"
+import { Chat } from "./chat.model"
+
+class ChatService extends BaseService {
+    public async getChat(authuser: jwtCred) {
+        const user_id = authuser.id
+        return this.internalResponse(
+            true,
+            {
+                /** replace with data */
+            },
+            200,
+            "Chat retrieved"
+        )
+    }
+
+    public async newChat(authuser: jwtCred, chatDTO: {}) {
+        const user_id = authuser.id
+
+        return this.internalResponse(
+            true,
+            {
+                /** replace with data */
+            },
+            200,
+            "New chat created"
+        )
+    }
+}
+
+export const chatService = new ChatService()
---
+++ src/db/migration/1631511907893-MessageTable.ts
@@ -0,0 +1,79 @@
+import {
+    MigrationInterface,
+    QueryRunner,
+    Table,
+    TableColumn,
+    TableForeignKey,
+} from "typeorm"
+
+export class MessageTable1631511907893 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "message",
+                columns: [
+                    {
+                        name: "id",
+                        type: "int",
+                        isPrimary: true,
+                    },
+
+                    {
+                        name: "content",
+                        type: "varchar",
+                    },
+
+                    {
+                        name: "type",
+                        type: "varchar",
+                    },
+                    {
+                        name: "sender_id",
+                        type: "int",
+                    },
+
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "CURRENT_TIMESTAMP(6)",
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestamp",
+                        default: "CURRENT_TIMESTAMP(6)",
+                        onUpdate: "CURRENT_TIMESTAMP(6)",
+                    },
+                ],
+            }),
+            true
+        )
+
+        await queryRunner.addColumn(
+            "message",
+            new TableColumn({
+                name: "chatId",
+                type: "int",
+                isNullable: true,
+            })
+        )
+
+        await queryRunner.createForeignKeys("message", [
+            new TableForeignKey({
+                columnNames: ["chatId"],
+                referencedColumnNames: ["id"],
+                referencedTableName: "chat",
+                onDelete: "CASCADE",
+            }),
+        ])
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        const table = await queryRunner.getTable("message")
+        const foreignKey = table.foreignKeys.find(
+            (fk) => fk.columnNames.indexOf("chatId") !== -1
+        )
+        await queryRunner.dropForeignKey("message", foreignKey)
+        await queryRunner.dropColumn("message", "chatId")
+        await queryRunner.dropTable("message")
+    }
+}
