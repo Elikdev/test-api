99f1e85d2e37958fcf8ead9f78c6982eb01f3a99
bemijonathan <bemijonathan@gmail.com>
2021-09-27 17:04:38 +0100

    fix some errors in orm config

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,10 @@
 const dotenv = require("dotenv");
 
-if (process.env.NODE_ENV === 'production') {
-	dotenv.config()
-} else {
-	dotenv.config({ path: '.dev.env' })
-}
+// if (process.env.NODE_ENV === 'production'){
+dotenv.config()
+// }else {
+//    dotenv.config({ path: '.dev.env'})
+// }
 
 module.exports = {
 	"type": "postgres",
