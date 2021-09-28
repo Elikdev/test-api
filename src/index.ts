26a272d83e00979a794344fd625cfb01de582137
bemijonathan <bemijonathan@gmail.com>
2021-09-28 04:46:57 +0100

    commit -m dixes

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,24 @@
 import "reflect-metadata";
 import * as dotenv from 'dotenv';
 
-if (process.env.NODE_ENV === 'production'){
-    dotenv.config()
-}else {
-    dotenv.config({ path: '.local.env'})
-}
+// if (process.env.NODE_ENV === 'production'){
+dotenv.config()
+// }else {
+//     dotenv.config({ path: '.local.env'})
+// }
 
 import app from "./app";
 import * as dbConfig from '../ormconfig.js'
-import {createConnection} from "typeorm";
+import { createConnection } from "typeorm";
 
 createConnection(dbConfig).then(async connection => {
     console.log("Here you can setup and run express/koa/any other framework.");
 }).catch(error => console.log(error));
 
-const PORT : string | number = process.env.PORT || 3000
+const PORT: string | number = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-    console.log("app is listening 🚀 on ", PORT )
+    console.log("app is listening 🚀 on ", PORT)
 })
 
 
