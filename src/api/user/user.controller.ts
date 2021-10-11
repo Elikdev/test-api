7b0f92d0fce8d4c862d93a13c576c0f4828746f2
neymarjimoh <jemohkunle2007@gmail.com>
2021-10-11 14:05:53 +0100

    added user update password

--- src/api/user/user.controller.ts
+++ src/api/user/user.controller.ts
@@ -28,4 +28,26 @@ userRouter.post("/update-profile", verificationMiddleware.validateToken, userVal
 }
 })
 
+userRouter.post("/change-password", verificationMiddleware.validateToken, userValidation.changePasswordValidation(), async (req: Request, res: Response) => {
+  try {
+   const authUser = (req as any).user
+ 
+   //service is being called here
+   const response =  await userService.changePassword(authUser, req.body)
+ 
+   if (!response.status) {
+     return errorResponse(res, response.message, 400)
+   }
+ 
+   return successRes(res, response.data, response.message)
+ 
+  } catch (error) {
+   console.log(error)
+   if (error?.email_failed) {
+     return errorResponse(res, 'Error in sending email. Contact support for help', 400)
+   }
+   return errorResponse(res, 'an error occured, contact support', 500)
+ }
+ })
+
 export default userRouter
--- src/middlwares/validations/user.validation.ts
+++ src/middlwares/validations/user.validation.ts
@@ -2,14 +2,28 @@ import { celebrate } from "celebrate";
 import Joi from "joi";
 
 class UserValidation {
- public updateProfileValidation() {
-  return celebrate({
-   body: {
-    profile_image: Joi.string().uri().optional(),
-    bio: Joi.string().optional()
-   }
-  })
- }
+  public updateProfileValidation() {
+    return celebrate({
+      body: {
+        profile_image: Joi.string().uri().optional(),
+        bio: Joi.string().optional(),
+      },
+    });
+  }
+
+  public changePasswordValidation() {
+    return celebrate({
+      body: {
+        old_password: Joi.string().required(),
+        new_password: Joi.string().min(8).invalid(Joi.ref("old_password")).required(),
+        confirm_password: Joi.any()
+          .equal(Joi.ref("new_password"))
+          .required()
+          .label("Confirm password")
+          .options({ messages: { "any.only": "{{#label}} must match new password" } }),
+      },
+    });
+  }
 }
 
-export const userValidation = new UserValidation()
+export const userValidation = new UserValidation();
