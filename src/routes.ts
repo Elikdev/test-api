a111df97c4edf85ea12db33f547c5be0fe2e71e5
Amakiri Samuel Bomate <amakirisamuelboma@gmail.com>
2021-10-28 20:47:10 +0100

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -8,7 +8,7 @@ import walletRouter from "./api/wallet/wallet.controller"
 import ratingRouter from "./api/ratings/ratings.controller"
 import industryRouter from "./api/industry/industry.controller"
 import transactionRouter from "./api/transactions/transaction.controller";
-// import tempRouter from "./api/temp/temp.controller";
+import tempRouter from "./api/temp/temp.controller";
 
 const apiPrefix = "/api/v1";
 
@@ -49,10 +49,10 @@ const routes = [
     prefix: "industry",
     name: industryRouter,
   },
-  // {
-  //   prefix: "temp",
-  //   name: tempRouter
-  // }
+  {
+    prefix: "temp",
+    name: tempRouter
+  }
   {
     prefix: "transaction",
     name: transactionRouter,
